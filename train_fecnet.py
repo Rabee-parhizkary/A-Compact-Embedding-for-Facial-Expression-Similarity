257a5cbdae678017b1e1a7ff8bed015c5d912dc1
shndap <sahandap@gmail.com>
2024-06-28 13:43:48 +0330

    Updated initial model

--- train_fecnet.py
+++ train_fecnet.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -16,53 +18,14 @@ from eval_metrics import evaluate
 from models.ModelDenseNet import DenseNet3
 
 
-# import imp
-
-# l2_dist = PairwiseDistance(2)
-# use_cuda = torch.cuda.is_available()
-# print('starting on cuda = ' + str(use_cuda))
-#
-# transform = transforms.Compose([# transforms.Grayscale(),
-#     transforms.Resize((224, 224)), transforms.ToTensor()])
-#
-# trainset = datas.fec_data.FecData(transform)
-# testset = datas.fec_data.FecTestData(transform)
-# trainloader = data.DataLoader(trainset, batch_size=24, num_workers=16)
-# testloader = data.DataLoader(testset, batch_size=20, num_workers=16)
-#
-# print('Finished loading data')
-#
-# net = KitModel()
-# # net = DenseNet(growthRate=12, depth=30, reduction=0.5,
-# #                         bottleneck=True, nClasses=16)
-#
-# # net = visionmodel.densenet121(pretrained=True)
-# # print(net)
-# # net.classifier = nn.Linear(net.classifier.in_features, 16)
-#
-# if use_cuda:
-#     net.cuda()
-#
-# total_epoch = 300
-# init_lr = 0.0005
-#
-# criterion = nn.TripletMarginLoss(margin=0.2)
-# optimizer = optim.SGD(params=net.parameters(), lr=init_lr * 50, momentum=0.9, weight_decay=5e-4)
-
-
-# ignored_params = list(map(id, net.classifier.parameters()))
-# base_params = filter(lambda p: id(p) not in ignored_params, net.parameters())
-# optimizer = optim.SGD([
-#     {'params': base_params},
-#     {'params': net.classifier.parameters(), 'lr': init_lr*50}], lr=init_lr, momentum=0.9, weight_decay=5e-4)
-
 # Training
-def train(epoch, net, use_cuda, trainloader, optimizer, criterion):
+def train(epoch, net, use_cuda, trainloader, optimizer, criterion, model_name):
     print("train {} epoch".format(epoch))
     labels, distances = [], []
     triplet_loss_sum = 0.0
     net.train()
-    for batch_idx, (anc, pos, neg) in tqdm(enumerate(trainloader)):
+    num = len(trainloader)
+    for batch_idx, (anc, pos, neg) in tqdm(enumerate(trainloader), total=num, desc=f'Epoch {epoch}'):
         if use_cuda:
             anc, pos, neg = anc.cuda(), pos.cuda(), neg.cuda()
         optimizer.zero_grad()
@@ -84,7 +47,14 @@ def train(epoch, net, use_cuda, trainloader, optimizer, criterion):
         labels.append(np.zeros(dists.size(0)))
         triplet_loss_sum += loss.item()
 
-    torch.save(net, "./check_points/triplet_v5_{}_checkpoint.pkl".format(epoch))
+    check_points_dir = ".\\check_points\\" + model_name
+    check_points_path = os.path.join(check_points_dir, "triplet_v5_{}_checkpoint.pkl".format(epoch))
+
+    # print(path)
+    if not os.path.exists(check_points_dir):
+        os.makedirs(check_points_dir)
+
+    torch.save(net, check_points_path)
     avg_triplet_loss = triplet_loss_sum / trainset.__len__()
     labels = np.array([sublabel for label in labels for sublabel in label])
     distances = np.array([subdist for dist in distances for subdist in dist])
@@ -94,13 +64,23 @@ def train(epoch, net, use_cuda, trainloader, optimizer, criterion):
     # print('  train set - Triplet Loss       = {:.8f}'.format(avg_triplet_loss))
     # print('  train set - Accuracy           = {:.8f}'.format(np.mean(accuracy)))
 
-    with open("./log/tune_v5_16.log", "a+") as log_file:
+    log_dir = ".\\log\\" + model_name
+    log_path = os.path.join(log_dir, "tune_v5_16.log")
+
+    # Create the directory if it does not exist
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # Open the log file and append the log entry
+    with open(log_path, "a+") as log_file:
         log_file.write(
-            "epoch: {0}, Triplet Loss: {1}, Accuracy: {2} \n".format(epoch, avg_triplet_loss, np.mean(accuracy)))
+            "epoch: {0}, Triplet Loss: {1}, Accuracy: {2} \n".format(epoch, avg_triplet_loss, np.mean(accuracy))
+        )
 
 
 if __name__ == '__main__':
-    torch.cuda.empty_cache()
+    models.Cleaner.clean_with_report()
+    # torch.cuda.empty_cache()
     torch.multiprocessing.freeze_support()
 
     l2_dist = PairwiseDistance(2)
@@ -108,30 +88,31 @@ if __name__ == '__main__':
     print('starting on cuda = ' + str(use_cuda))
 
     transform = transforms.Compose([  # transforms.Grayscale(),
-        transforms.Resize((224, 224)), transforms.ToTensor()])
+        transforms.Resize((100, 100)), transforms.ToTensor()])
 
-    trainset = datas.fec_data.FecData(datas.fec_data.path_pd, transform)
-    testset = datas.fec_data.FecData(datas.fec_data.path_test, transform)
-    trainloader = data.DataLoader(trainset, batch_size=24, num_workers=0)
-    testloader = data.DataLoader(testset, batch_size=20, num_workers=16)
+    coefficient_size = 0.01
+    train_size_init = 12414
+    test_size_init = 18954
+    test_size = int(test_size_init * coefficient_size)
+    train_size = int(train_size_init * coefficient_size)
+    trainset = datas.fec_data.FecData(datas.fec_data.path_pd, transform, size=train_size)
+    testset = datas.fec_data.FecData(datas.fec_data.path_test, transform, size=test_size)
+    trainloader = data.DataLoader(trainset, batch_size=20, num_workers=0)
+    testloader = data.DataLoader(testset, batch_size=20, num_workers=0)
 
     print('Finished loading data')
 
-    net = DenseNet3(growth_rate=32, depth=120, reduction=0.5,
+    net = DenseNet3(growth_rate=32, depth=30, reduction=0.5,
                     bottleneck=True, num_classes=16)
-
-    # net = vision_model.densenet121(pretrained=True)
-    # print(len(net))
 
     if use_cuda:
         net.cuda()
 
-    total_epoch = 300
+    total_epoch = 1
     init_lr = 0.0005
 
     criterion = nn.TripletMarginLoss(margin=0.2)
     optimizer = optim.SGD(params=net.parameters(), lr=init_lr * 50, momentum=0.9, weight_decay=5e-4)
 
-    for i in range(1):
-        train(i, net, use_cuda, trainloader, optimizer, criterion)
-
+    for i in tqdm(range(total_epoch)):
+        train(i, net, use_cuda, trainloader, optimizer, criterion, model_name='Dense Net')
